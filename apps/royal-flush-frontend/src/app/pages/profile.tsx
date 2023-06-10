4a2b73691dc023d48ab0c6cbeb99378c68a456a6
= <=>
2023-06-10 15:30:05 +0100

    semi ready session hook

--- apps/royal-flush-frontend/src/app/pages/profile.tsx
+++ apps/royal-flush-frontend/src/app/pages/profile.tsx
@@ -1,19 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { HOME_AXIOS_CLIENT } from '../app';
+import useSession from '../../hooks/useSession';
 
 const Profile = () => {
-  const navigate = useNavigate();
-  const { data:user, } = useQuery(['profile-user-me'], async () => {
-    const response = await HOME_AXIOS_CLIENT.get(`/home/me`, {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
-    });
-    return response.data;
-  });
-
+  const {userData} = useSession({
+    redirectTo: '/login',
+  })
   return (
     <div className="container mx-auto p-6">
       <div className="w-full max-w-2xl mx-auto rounded-md shadow-md overflow-hidden">
@@ -27,7 +20,7 @@ const Profile = () => {
         <div className="bg-white py-6 px-8">
           <h3 className="text-2xl font-medium text-gray-700">User details:</h3>
           <div className="mt-4 border-t border-gray-200 pt-4">
-            <pre className="text-sm text-gray-500">{JSON.stringify(user, null, 2)}</pre>
+            <pre className="text-sm text-gray-500">{JSON.stringify(userData, null, 2)}</pre>
           </div>
         </div>
       </div>
--- apps/royal-flush-frontend/src/hooks/useSession.ts
+++ apps/royal-flush-frontend/src/hooks/useSession.ts
@@ -1,8 +1,11 @@
 import { UserType } from "@royal/shared";
-import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { HOME_AXIOS_CLIENT } from "../app/app";
 
 type UseSessionOptions = {
-    redirectTo?: string;
+    redirectTo: string;
     redirectIfFound?: boolean;
 };
 type IUseSession = {
@@ -12,8 +15,20 @@ type IUseSession = {
 export default function useSession({
     redirectTo,
     redirectIfFound,
-}: UseSessionOptions = {'redirectTo': '/', 'redirectIfFound': false}): IUseSession {
-    const [userData, setUserData] = useState<Omit<UserType, 'password'> | undefined>();
-    
-    return { userData };
+}: UseSessionOptions = {redirectTo: '/login', redirectIfFound: false}): IUseSession {
+    const navigate = useNavigate();
+    const { data:user, } = useQuery(['profile-user-me'], async () => {
+      const response = await HOME_AXIOS_CLIENT.get(`/home/me`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      if(response.status === 401) {
+        localStorage.removeItem('token');
+        navigate(redirectTo);
+      }
+      return response.data as Omit<UserType, 'password'>;
+    });
+        
+    return { userData: user };
 }
